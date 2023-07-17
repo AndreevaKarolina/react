33698e892e021a8bbdeb347357b7784b0812f81c
AndreevaKarolina <andreevakarolona@gmail.com>
2023-07-18 00:10:10 +0300

    коммит

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import { Chart } from "react-google-charts";
 import './App.css';
+
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Образование", 108],
+  ["ЖКХ", 94],
+  ["Дороги",77],
+  ["Стоительство", 57],
+  ["Экология", 38],
+  ["Медицина", 24],
+  ["Общественный транспорт", 18],
+  ["Культура", 10],
+  ["Спорт", 7],
+];
+
+export const options = {
+  title: "ТОП-10 СФЕР",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Chart 
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+      />
+        </>
+      
   );
 }
 
